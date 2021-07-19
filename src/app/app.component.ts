7133aa6d4dc64be9d10611601dfac2996a2a4cd9
chinifabio <fabio.chini@mail.polimi.it>
2021-07-19 16:09:14 +0200

    Implemented operations.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { KeyComponent } from './components/key/key.component';
+import { Component } from "@angular/core";
+import {isNumeric} from "rxjs/internal-compatibility";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
 
+  num = 0;
+  res = 0;
+  temp = 0;
+
   title = "calculator";
-  chars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '+', '-', '*', '/'];
-  gino = "";
 
-  pressed(digit: any) {
-    this.gino = digit == '0' ? "" : "you pressed " + digit;
+  chars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  operand = ["+", "-", "*", "/"];
+
+  operation = "+";
+
+  pressed(input: string) {
+    if(isNumeric(input)) {
+      this.num*=10; this.num += Number.parseInt(input);
+
+      switch (this.operation) {
+        case "*":
+          this.temp = this.res * this.num;
+          break;
+        case "+":
+          this.temp = this.res + this.num;
+          break;
+        case "-":
+          this.temp = this.res - this.num;
+          break;
+        case "/":
+          this.temp = this.res / this.num;
+          break;
+      }
+    }
+
+    else {
+      this.res = this.temp;
+      this.num = 0;
+      this.operation = input;
+      console.log(this.operation)
+    }
   }
-
 }
